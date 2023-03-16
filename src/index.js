c52b52c6ee8b3fd1ee249feced85c39b022c4de8
AleksaLuki <dimirtiy9000@gmail.com>
2023-03-16 12:34:18 +0200

    newfile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
+
